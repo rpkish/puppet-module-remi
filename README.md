3cf4332818f564d103d03f5a1e0e9b31540b80da
Chris Hall <christopher.hall@threattrack.com>
2013-05-16 13:32:05 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-puppet-module-remi
-==================
+# Configure remi repository
 
-Puppet module for remi repository
+# About
+This module basically just mimics the remi-release rpm.
+
+The following Repos will be setup and enabled by default:
+
+  * remi
+
+Other repositories that will setup but disabled (as per the remi-release setup)
+
+  * remi-debuginfo
+  * remi-test
+  * remi-test-debuginfo
+
+# Futher Information
+
+* [remi website](http://rpms.famillecollet.com/)
+
+# Testing
+
+* This was tested using Puppet 3.1.x on Centos6
+* I assume it will work on any RHEL variant
+
+# License
+Apache Software License 2.0
---
+++ manifests/init.pp
@@ -0,0 +1,66 @@
+# Class remi
+#
+# Actions:
+#   Configure the proper repositories and import GPG keys
+#
+# Requires:
+#   You should probably be on an Enterprise Linux variant. (Centos, RHEL, Scientific, Oracle, Ascendos, et al)
+#
+# Dependencies:
+#   stahnma/epel module, version: '0.0.3'
+#
+# Sample Usage:
+#   include remi
+#
+
+class remi {
+
+  if $::osfamily == 'RedHat' and $::operatingsystem != 'Fedora' {
+
+    yumrepo { 'remi':
+      descr          => "Les RPM de remi pour Enterprise Linux ${::is_maj_version} - ${::architecture}",
+      mirrorlist     => "http://rpms.famillecollet.com/enterprise/${::os_maj_version}/remi/mirror",
+      enabled        => '1',
+      gpgcheck       => '1',
+      gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi",
+    }
+
+    yumrepo { 'remi-test':
+      descr          => "Les RPM de remi pour Enterprise Linux 6 - ${::architecture}",
+      mirrorlist     => "http://rpms.famillecollet.com/enterprise/${::os_major_version}/test/mirror",
+      enabled        => '0',
+      gpgcheck       => '1',
+      gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi",
+    }
+
+    yumrepo { 'remi-debuginfo':
+      descr          => "Les RPM de remi pour Enterprise Linux ${::os_maj_version} - ${::architecture} - debuginfo",
+      baseurl        => "http://rpms.famillecollet.com/enterprise/${::os_maj_version}/debug-remi/${::architecture}/",
+      enabled        => '0',
+      gpgcheck       => '1',
+      gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi",
+    }
+
+    yumrepo { 'remi-test-debuginfo':
+      descr          => "Les RPM de remi en test pour Enterprise Linux ${::os_maj_version} - ${::architecture} - debuginfo",
+      baseurl        => "http://rpms.famillecollet.com/enterprise/${::os_maj_version}/debug-test/${::architecture}/",
+      enabled        => '0',
+      gpgcheck       => '1',
+      gpgkey         => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi",
+    }
+
+    file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi":
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => "puppet:///modules/remi/RPM-GPG-KEY-remi",
+    }
+
+    remi::rpm_gpg_key{ "remi":
+      path => "/etc/pki/rpm-gpg/RPM-GPG-KEY-remi"
+    }
+  } else {
+      notice ("Your operating system ${::operatingsystem} will not have the REMI repository applied")
+  }
+}
